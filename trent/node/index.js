ab1d6bf68cf970bd97aeca0a4cddb3c2abc53ae0
Trent <devtrentoh@gmail.com>
2017-08-14 02:34:21 +0900

    Database Tutorial

--- trent/node/index.js
+++ trent/node/index.js
@@ -1,21 +1,73 @@
 // index.js
-const path = require('path')
-const express = require('express')
-const exphbs = require('express-handlebars')
 
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
-app.get('/', (request, response) => {
-  response.render('home', {
-    name: 'John'
+const pg = require('pg')
+const conString = 'postgres://minsik:testpass@localhost/node_hero'
+
+// pg.connect(conString, function (err, client, done) {
+//   if (err) {
+//     return console.error('error fetching client from pool', err)
+//   }
+//   client.query('SELECT $1::varchar AS my_first_query',
+//     ['node hero'], function(err, result) {
+//       done()
+//
+//       if (err) {
+//         return console.error('error happend during query', err)
+//       }
+//       console.log(result.rows[0])
+//       process.exit(0)
+//     })
+// })
+
+app.use(bodyParser.json());
+
+// curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "Trent", "age": "17"}'
+
+app.post('/users', function (req, res, next) {
+  const user = req.body
+
+  pg.connect(conString, function (err, client, done) {
+    if (err) {
+      // error passed to express error handler
+      return next(err)
+    }
+    client.query(
+      'INSERT INTO users (name, age) VALUES ($1, $2);',
+      [user.name, user.age], function (err, result) {
+        done() // signalling that connection can be closed
+
+        if (err) {
+          return next(err)
+        }
+
+        res.send(200)
+      }
+    )
+  }
+  )
+})
+app.get('/users', function (req, res, next) {
+  pg.connect(conString, function (err, client, done) {
+    if (err) {
+      return next(err)
+    }
+    client.query('SELECT name, age FROM users;', [],
+      function (err, result) {
+        done()
+
+        if (err) {
+          // error passed to express error handler
+          return next(err)
+        }
+
+        res.json(result.rows)
+      })
   })
 })
-
 app.listen(3000)
