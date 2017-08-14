ba62c7d201b1f0c6768b390d6cba883b6ecfb76d
Trent <devtrentoh@gmail.com>
2017-08-14 22:31:56 +0900

    Request-Promise Tutorial 1

--- trent/node/index.js
+++ trent/node/index.js
@@ -1,73 +1,29 @@
 // index.js
 
-'use strict'
+const request = require('request-promise')
 
-const express = require('express')
-const bodyParser = require('body-parser');
-const app = express()
-
-const pg = require('pg')
-const conString = 'postgres://minsik:testpass@localhost/node_hero'
-
-// pg.connect(conString, function (err, client, done) {
-//   if (err) {
-//     return console.error('error fetching client from pool', err)
-//   }
-//   client.query('SELECT $1::varchar AS my_first_query',
-//     ['node hero'], function(err, result) {
-//       done()
-//
-//       if (err) {
-//         return console.error('error happend during query', err)
-//       }
-//       console.log(result.rows[0])
-//       process.exit(0)
-//     })
-// })
-
-app.use(bodyParser.json());
-
-// curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "Trent", "age": "17"}'
-
-app.post('/users', function (req, res, next) {
-  const user = req.body
-
-  pg.connect(conString, function (err, client, done) {
-    if (err) {
-      // error passed to express error handler
-      return next(err)
-    }
-    client.query(
-      'INSERT INTO users (name, age) VALUES ($1, $2);',
-      [user.name, user.age], function (err, result) {
-        done() // signalling that connection can be closed
-
-        if (err) {
-          return next(err)
-        }
-
-        res.send(200)
-      }
-    )
+const options = {
+  method: 'GET',
+  uri: 'https://risingstack.com',
+  json: true,
+  qs: {
+    limit: 10,
+    skip: 20,
+    sort: 'asc'
+  },
+  headers: {
+    'User-Agent': 'Request-Promise',
+    'Authorization': 'Basic QWxhZGRpbjpPcGVuU2VzYW1l'
   }
-  )
-})
-app.get('/users', function (req, res, next) {
-  pg.connect(conString, function (err, client, done) {
-    if (err) {
-      return next(err)
-    }
-    client.query('SELECT name, age FROM users;', [],
-      function (err, result) {
-        done()
+}
+// https://risingstack.com?limit=10&skip=20&sort=asc
 
-        if (err) {
-          // error passed to express error handler
-          return next(err)
-        }
-
-        res.json(result.rows)
-      })
+request(options)
+  .then(function (response) {
+    // Successful request
+    console.log(response)
   })
-})
-app.listen(3000)
+  .catch(function (err) {
+    // Error handling
+    console.log(err)
+  })
